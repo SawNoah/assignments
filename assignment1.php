8357bbc8220bf6057871bb03ce3bb199b606a5a1
Saw Noah <sawnoah2002@gmail.com>
2023-06-04 18:35:27 +0630

    Calculating BMI

---
+++ assignment1.php
@@ -0,0 +1,30 @@
+<?php
+//Calculating BMI
+
+$myWeight = 153;
+$myHeight = 68;
+$BMI = 703 * $myWeight/ ($myHeight)^2;
+
+echo "Your Weight(lb) is : $myWeight.\n";
+echo "<br/>";
+echo "Your Height(in) is : $myHeight.\n";
+echo "<br/>";
+echo "Your BMI is : $BMI.\n";
+echo "<br/>";
+
+if ($BMI < 18.5):
+    echo "You are Underweight.\n";
+
+elseif ($BMI >= 18.5 && $BMI <= 24.9):
+    echo "You are Normalweight.\n";
+
+elseif ($BMI >=25 && $BMI <=29.9):
+    echo "You are Overweight.\n";
+
+elseif ($BMI >= 30 && $BMI <= 34.9):
+    echo "You are Obese.\n";
+
+else:
+    echo "You are Extremely Obese.\n";
+
+?>
