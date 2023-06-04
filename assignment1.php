893815dbf79aff8b8fe9bed0729713589794e8ee
Saw Noah <sawnoah2002@gmail.com>
2023-06-04 18:45:11 +0630

    Add functions

--- assignment1.php
+++ assignment1.php
@@ -5,26 +5,39 @@ $myWeight = 153;
 $myHeight = 68;
 $BMI = 703 * $myWeight/ ($myHeight)^2;
 
-echo "Your Weight(lb) is : $myWeight.\n";
-echo "<br/>";
-echo "Your Height(in) is : $myHeight.\n";
-echo "<br/>";
-echo "Your BMI is : $BMI.\n";
-echo "<br/>";
-
-if ($BMI < 18.5):
-    echo "You are Underweight.\n";
-
-elseif ($BMI >= 18.5 && $BMI <= 24.9):
-    echo "You are Normalweight.\n";
-
-elseif ($BMI >=25 && $BMI <=29.9):
-    echo "You are Overweight.\n";
-
-elseif ($BMI >= 30 && $BMI <= 34.9):
-    echo "You are Obese.\n";
-
-else:
-    echo "You are Extremely Obese.\n";
-
+function Info()
+{
+    echo "Your Weight(lb) is : $myWeight.\n";
+    echo "<br/>";
+    echo "Your Height(in) is : $myHeight.\n";
+    echo "<br/>";
+    echo "Your BMI is : $BMI.\n";
+    echo "<br/>";
+    break;
+}
+function Status()
+{
+    if ($BMI < 18.5)
+    {
+        echo "You are Underweight.\n";
+    }
+    elseif ($BMI >= 18.5 && $BMI <= 24.9)
+    {
+        echo "You are Normalweight.\n";
+    }
+    elseif ($BMI >=25 && $BMI <=29.9)
+    {
+        echo "You are Overweight.\n";
+    }
+    elseif ($BMI >= 30 && $BMI <= 34.9)
+    {
+        echo "You are Obese.\n";
+    }  
+    else
+    {
+        echo "You are Extremely Obese.\n";
+    }
+    echo "<br/>";
+break;            
+}
 ?>
