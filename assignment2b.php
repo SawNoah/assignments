84b09649cb90834cd3d4db182fc2f63ef7aa4e24
Saw Noah <sawnoah2002@gmail.com>
2023-06-12 17:34:56 +0630

    Fix errors

--- assignment2b.php
+++ assignment2b.php
@@ -42,17 +42,17 @@ class Information
         {
             echo "Minimum age 12 is required to join the school. <br>";
         }
-        elseif (12 <= $this->age <15)
+        elseif (12 <= $this->age && $this->age <15)
         {
-            echo "Assign to Classroom No.1.<br>";
+            echo "Assign to Classroom No.1. <br>";
         }
-        elseif (15 <= $this->age <18)
+        elseif (15 <= $this->age && $this->age <18)
         {
-            echo "Assign to Classroom No.2.<br>";
+            echo "Assign to Classroom No.2. <br>";
         }
         else
         {
-            echo "Over the age 18 is unable to join the school.<br>";
+            echo "Over the age 18 is unable to join the school. <br>";
         }
     }
 }
@@ -61,8 +61,8 @@ $student1 = new Student("Mg Mg");
 $student1Info = $student1->getContent();
 $student1Info->Gender("male");
 $student1Info->Age(16);
-
+echo "<br>";
 $student2 = new Student("Sarah");
 $student2Info = $student2->getContent();
 $student2Info->Gender("female");
-$student2Info->Age(15);
+$student2Info->Age(14);
